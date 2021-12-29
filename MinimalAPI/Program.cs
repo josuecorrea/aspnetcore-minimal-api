05aa735d9a1283656ab3665a3d957cb90a0a6edb
Josue Correa <josuejtc@gmail.com>
2021-12-29 15:09:55 -0300

    AddStackExchangeRedisCache

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,14 +1,26 @@
+using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+    options.InstanceName = "RedisServer";
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 
-app.MapGet("/", () => "Hello World!");
+var cache = app.Services.GetService<IDistributedCache>();
+
+await cache.SetStringAsync("value_one", "First mesage test!");
+
+app.MapGet("/", async () => await cache.GetStringAsync("value_one"));
 
 app.UseSwaggerUI();
 
